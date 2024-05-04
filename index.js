7a3161926c19a270b9cd300abd93fba0255497e0
Athena Maynard <xfifix17@gmail.com>
2024-05-04 18:13:05 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+
+function moveDodgerRight()  {const leftNumbers = dodger.style.left.replace("px", "180");
+const left = parseInt(leftNumbers, 10);
+
+if (left > 0) {
+  dodger.style.left = `${left + 1}px`;
+}
+};
+
+function moveDodgerLeft(){  const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
+
+if (left > 0) {
+  dodger.style.left = `${left - 1}px`;
+
+}
+};
+
